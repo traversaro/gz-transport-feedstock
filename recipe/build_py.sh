741edca1ede1c7e81b3166057a5353eea342ed3a
Silvio Traversaro <silvio@traversaro.it>
2024-11-11 17:06:29 +0100

    Do not use workarounds to pass include Python directories to CMake

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -11,13 +11,15 @@ rm -rf build
 mkdir build
 cd build
 
+Python3_INCLUDE_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("include"))')"
+
 cmake ${CMAKE_ARGS} -GNinja .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DUSE_SYSTEM_PATHS_FOR_PYTHON_INSTALLATION:BOOL=ON \
     -DPython3_EXECUTABLE:PATH=$PYTHON \
     -DPYTHON_EXECUTABLE:PATH=$PYTHON \
-    -DPython3_INCLUDE_DIR:PATH=$PREFIX/include/`ls $PREFIX/include | grep "python\|pypy"` \
+    -DPython3_INCLUDE_DIR:PATH=$Python3_INCLUDE_DIR \
     -DBUILD_TESTING:BOOL=ON
 
 cmake --build . --config Release
